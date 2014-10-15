787956222f8e96b7f10eee78fe4903c15af6724d
Mathilde <mathilde.maltaire@gmail.com>
2014-10-15 17:33:09 +0200

    lecture dico

--- src/T1.java
+++ src/T1.java
@@ -1,14 +1,8 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.FileReader;
+import java.io.IOException;
 
-/**
- * 
- */
+
 
 /**
  * @author Mathilde
@@ -21,39 +15,33 @@ public class T1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		String chaine="";
-		String fichier ="dico.tsv";
-		
-		//lecture du fichier texte	
-		try{
-			InputStream ips=new FileInputStream(fichier); 
-			InputStreamReader ipsr=new InputStreamReader(ips);
-			BufferedReader br=new BufferedReader(ipsr);
-			String ligne;
-			while ((ligne=br.readLine())!=null){
-				System.out.println(ligne);
-				chaine+=ligne+"\n";
-			}
-			br.close(); 
-		}		
-		catch (Exception e){
-			System.out.println(e.toString());
-		}
-	
-		//création ou ajout dans le fichier texte
-		try {
-			FileWriter fw = new FileWriter (fichier);
-			BufferedWriter bw = new BufferedWriter (fw);
-			PrintWriter fichierSortie = new PrintWriter (bw); 
-				fichierSortie.println (chaine+"\n test de lecture et écriture !!"); 
-			fichierSortie.close();
-			System.out.println("Le fichier " + fichier + " a été créé!"); 
-		}
-		catch (Exception e){
-			System.out.println(e.toString());
-		}		
-	}
-		// TODO Auto-generated method stub
 
+		String filePath = "src/dico.tsv";
+		 
+		try{
+		// Création du flux bufférisé sur un FileReader, immédiatement suivi par un 
+		// try/finally, ce qui permet de ne fermer le flux QUE s'il le reader
+		// est correctement instancié (évite les NullPointerException)
+		BufferedReader buff = new BufferedReader(new FileReader(filePath));
+		 
+		try {
+		String line;
+		// Lecture du fichier ligne par ligne. Cette boucle se termine
+		// quand la méthode retourne la valeur null.
+		while ((line = buff.readLine()) != null) {
+				//System.out.println(line);
+				//faites ici votre traitement
+		}
+		
+		} finally {
+			// dans tous les cas, on ferme nos flux
+			buff.close();
+		}
+		} catch (IOException ioe) {
+			// erreur de fermeture des flux
+			System.out.println("Erreur --" + ioe.toString());
+		}
+
+		
 	}
+}
