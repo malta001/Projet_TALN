a97e89c8b4bd2243c7ca9194a2aa1ec4c5498412
Mathilde <mathilde.maltaire@gmail.com>
2014-10-15 16:10:16 +0200

    Lecture dico

---
+++ src/T1.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+/**
+ * 
+ */
+
+/**
+ * @author Mathilde
+ *
+ */
+public class T1 {
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String chaine="";
+		String fichier ="dico.tsv";
+		
+		//lecture du fichier texte	
+		try{
+			InputStream ips=new FileInputStream(fichier); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			while ((ligne=br.readLine())!=null){
+				System.out.println(ligne);
+				chaine+=ligne+"\n";
+			}
+			br.close(); 
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+	
+		//création ou ajout dans le fichier texte
+		try {
+			FileWriter fw = new FileWriter (fichier);
+			BufferedWriter bw = new BufferedWriter (fw);
+			PrintWriter fichierSortie = new PrintWriter (bw); 
+				fichierSortie.println (chaine+"\n test de lecture et écriture !!"); 
+			fichierSortie.close();
+			System.out.println("Le fichier " + fichier + " a été créé!"); 
+		}
+		catch (Exception e){
+			System.out.println(e.toString());
+		}		
+	}
+		// TODO Auto-generated method stub
+
+	}
